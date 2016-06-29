f2c76cdf748bfe755e3d7c4b195f0ff9bc5176b8
Peter Burns <rictic@google.com>
2016-06-28 18:33:02 -0700

    Add a bunch of type annotations. Remove output wrapper

--- src/CustomElements/v1/CustomElements.js
+++ src/CustomElements/v1/CustomElements.js
@@ -49,10 +49,17 @@ var CustomElementDefinition;
 
   /** @const */
   var customNameValidation = /^[a-z][.0-9_a-z]*-[\-.0-9_a-z]*$/;
+
+  /**
+   * @param {string} name
+   */
   function isValidCustomElementName(name) {
     return customNameValidation.test(name) && reservedTagList.indexOf(name) === -1;
   }
 
+  /**
+   * @param {Node} root
+   */
   function createTreeWalker(root) {
     // IE 11 requires the third and fourth arguments be present. If the third
     // arg is null, it applies the default behaviour. However IE also requires
@@ -60,6 +67,9 @@ var CustomElementDefinition;
     return doc.createTreeWalker(root, NodeFilter.SHOW_ELEMENT, null, false);
   }
 
+  /**
+   * @param {Node} node
+   */
   function isElement(node) {
     return node.nodeType === Node.ELEMENT_NODE
   }
@@ -70,30 +80,32 @@ var CustomElementDefinition;
    * See https://html.spec.whatwg.org/multipage/scripting.html#customelementsregistry
    *
    * @constructor
-   * @property {boolean} polyfilled Whether this registry is polyfilled
-   * @property {boolean} enableFlush Set to true to enable the flush() method
-   *   to work. This should only be done for tests, as it causes a memory leak.
    */
   function CustomElementsRegistry() {
-    /** @private {Map<string, CustomElementDefinition>} **/
+    /** @private {!Map<string, !CustomElementDefinition>} **/
     this._definitions = new Map();
 
-    /** @private {Map<Function, CustomElementDefinition>} **/
+    /** @private {!Map<!Function, string>} **/
     this._constructors = new Map();
 
+    /** @private {!Map<string, !{promise: !Promise, resolve: !Function}>} */
     this._whenDefinedMap = new Map();
 
-    /** @private {Set<MutationObserver>} **/
+    /** @private {!Set<!MutationObserver>} **/
     this._observers = new Set();
 
-    /** @private {MutationObserver} **/
+    /** @private {!MutationObserver} **/
     this._attributeObserver =
         new MutationObserver(this._handleAttributeChange.bind(this));
 
-    /** @private {HTMLElement} **/
+    /** @private {?HTMLElement} **/
     this._newInstance = null;
 
+    /** @public {boolean} Whether this registry is polyfilled. */
     this.polyfilled = true;
+    /** @public {boolean} Set to true to enable the flush() method to work.
+     *    This should only be done for tests, as it causes a memory leak.
+     */
     this.enableFlush = false;
 
     this._observeRoot(document);
@@ -103,6 +115,11 @@ var CustomElementDefinition;
 
     // HTML spec part 4.13.4
     // https://html.spec.whatwg.org/multipage/scripting.html#dom-customelementsregistry-define
+    /**
+     * @param {string} name
+     * @param {!Function} constructor
+     * @param {{extends: string}} options
+     */
     define: function(name, constructor, options) {
       name = name.toString().toLowerCase();
 
@@ -231,14 +248,13 @@ var CustomElementDefinition;
       if (this._definitions.has(name)) {
         return Promise.resolve();
       }
-      var deferred = {
-        promise: null,
-      };
-      deferred.promise = new Promise(function(resolve, _) {
-       deferred.resolve = resolve;
-      });
-      this._whenDefinedMap.set(name, deferred);
-      return deferred.promise;
+      /** @type {!Function} */
+      var resolve;
+      var promise = new Promise(function(resolve, _) {
+       resolve = resolve;
+      })
+      this._whenDefinedMap.set(name, {promise, resolve});
+      return promise;
     },
 
     /**
@@ -278,10 +294,10 @@ var CustomElementDefinition;
     _unobserveRoot: function(root) {
       if (root.__observer) {
         root.__observer.disconnect();
-        root.__observer = null;
         if (this.enableFlush) {
           this._observers.delete(root.__observer);
         }
+        root.__observer = null;
       }
     },
 
@@ -475,75 +491,75 @@ var CustomElementDefinition;
 
   /** @const */
   var htmlElementSubclasses = [
-  	'Button',
-  	'Canvas',
-  	'Data',
-  	'Head',
-  	'Mod',
-  	'TableCell',
-  	'TableCol',
-    'Anchor',
-    'Area',
-    'Base',
-    'Body',
-    'BR',
-    'DataList',
-    'Details',
-    'Dialog',
-    'Div',
-    'DList',
-    'Embed',
-    'FieldSet',
-    'Form',
-    'Heading',
-    'HR',
-    'Html',
-    'IFrame',
-    'Image',
-    'Input',
-    'Keygen',
-    'Label',
-    'Legend',
-    'LI',
-    'Link',
-    'Map',
-    'Media',
-    'Menu',
-    'MenuItem',
-    'Meta',
-    'Meter',
-    'Object',
-    'OList',
-    'OptGroup',
-    'Option',
-    'Output',
-    'Paragraph',
-    'Param',
-    'Picture',
-    'Pre',
-    'Progress',
-    'Quote',
-    'Script',
-    'Select',
-    'Slot',
-    'Source',
-    'Span',
-    'Style',
-    'TableCaption',
-    'Table',
-    'TableRow',
-    'TableSection',
-    'Template',
-    'TextArea',
-    'Time',
-    'Title',
-    'Track',
-    'UList',
-    'Unknown',
+    'HTMLButtonElement',
+    'HTMLCanvasElement',
+    'HTMLDataElement',
+    'HTMLHeadElement',
+    'HTMLModElement',
+    'HTMLTableCellElement',
+    'HTMLTableColElement',
+    'HTMLAnchorElement',
+    'HTMLAreaElement',
+    'HTMLBaseElement',
+    'HTMLBodyElement',
+    'HTMLBRElement',
+    'HTMLDataListElement',
+    'HTMLDetailsElement',
+    'HTMLDialogElement',
+    'HTMLDivElement',
+    'HTMLDListElement',
+    'HTMLEmbedElement',
+    'HTMLFieldSetElement',
+    'HTMLFormElement',
+    'HTMLHeadingElement',
+    'HTMLHRElement',
+    'HTMLHtmlElement',
+    'HTMLIFrameElement',
+    'HTMLImageElement',
+    'HTMLInputElement',
+    'HTMLKeygenElement',
+    'HTMLLabelElement',
+    'HTMLLegendElement',
+    'HTMLLIElement',
+    'HTMLLinkElement',
+    'HTMLMapElement',
+    'HTMLMediaElement',
+    'HTMLMenuElement',
+    'HTMLMenuItemElement',
+    'HTMLMetaElement',
+    'HTMLMeterElement',
+    'HTMLObjectElement',
+    'HTMLOListElement',
+    'HTMLOptGroupElement',
+    'HTMLOptionElement',
+    'HTMLOutputElement',
+    'HTMLParagraphElement',
+    'HTMLParamElement',
+    'HTMLPictureElement',
+    'HTMLPreElement',
+    'HTMLProgressElement',
+    'HTMLQuoteElement',
+    'HTMLScriptElement',
+    'HTMLSelectElement',
+    'HTMLSlotElement',
+    'HTMLSourceElement',
+    'HTMLSpanElement',
+    'HTMLStyleElement',
+    'HTMLTableCaptionElement',
+    'HTMLTableElement',
+    'HTMLTableRowElement',
+    'HTMLTableSectionElement',
+    'HTMLTemplateElement',
+    'HTMLTextAreaElement',
+    'HTMLTimeElement',
+    'HTMLTitleElement',
+    'HTMLTrackElement',
+    'HTMLUListElement',
+    'HTMLUnknownElement',
   ];
 
   for (var i = 0; i < htmlElementSubclasses.length; i++) {
-    var ctor = window['HTML' + htmlElementSubclasses[i] + 'Element'];
+    var ctor = window[htmlElementSubclasses[i]];
     if (ctor) {
       ctor.prototype.__proto__ = win.HTMLElement.prototype;
     }
